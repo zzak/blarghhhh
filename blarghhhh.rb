34460bfe51812b017602a3e78bab5d8323cf5886
Zachary Scott <zachary.s.scott@gmail.com>
2010-08-28 00:35:35 -0400

    fix minor tab issue

--- blarghhhh.rb
+++ blarghhhh.rb
@@ -12,13 +12,13 @@ userid = 'zacharyscott'
 repoid = 'my_blarghhhh'
 
 get '/' do
-	@info = HTTParty.get("#{base_uri}repos/show/#{userid}/#{repoid}")
-	@blobs = HTTParty.get("#{base_uri}blob/all/#{userid}/#{repoid}/master")
+  @info = HTTParty.get("#{base_uri}repos/show/#{userid}/#{repoid}")
+  @blobs = HTTParty.get("#{base_uri}blob/all/#{userid}/#{repoid}/master")
   erb :index
 end
 
 get '/show/:post/:sha' do
-	@info = HTTParty.get("#{base_uri}repos/show/#{userid}/#{repoid}")
+  @info = HTTParty.get("#{base_uri}repos/show/#{userid}/#{repoid}")
   doc = HTTParty.get("#{base_uri}blob/show/#{userid}/#{repoid}/#{params[:sha]}").to_s
   @post = RDiscount.new(doc).to_html
   erb :show
