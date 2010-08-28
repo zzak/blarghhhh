5331e63116cbcec78debd1cf24fb23298be28984
Zachary Scott <zachary.s.scott@gmail.com>
2010-08-28 14:16:25 -0400

    adding some additional info about the repo via the api

--- blarghhhh.rb
+++ blarghhhh.rb
@@ -11,16 +11,21 @@ base_uri = 'http://github.com/api/v2/json/'
 userid = 'zacharyscott'
 repoid = 'my_blarghhhh'
 
+
 get '/' do
   @info = HTTParty.get("#{base_uri}repos/show/#{userid}/#{repoid}")
+  @collaborators = HTTParty.get("#{base_uri}repos/show/#{userid}/#{repoid}/collaborators")
   @blobs = HTTParty.get("#{base_uri}blob/all/#{userid}/#{repoid}/master")
   erb :index
 end
 
 get '/show/:post/:sha' do
   @info = HTTParty.get("#{base_uri}repos/show/#{userid}/#{repoid}")
+  @collaborators = HTTParty.get("#{base_uri}repos/show/#{userid}/#{repoid}/collaborators")
   doc = HTTParty.get("#{base_uri}blob/show/#{userid}/#{repoid}/#{params[:sha]}").to_s
   @post = RDiscount.new(doc).to_html
+  @current = HTTParty.get("#{base_uri}commits/show/#{userid}/#{repoid}/#{params[:sha]}")
+  @history = HTTParty.get("#{base_uri}commits/show/#{userid}/#{repoid}/master/#{params[:post]}")
   erb :show
 end
 
