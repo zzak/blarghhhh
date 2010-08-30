7783fd70d6869b42ccdcca4c0a45de8f8b1df2a9
Zachary Scott <zachary.s.scott@gmail.com>
2010-08-30 16:21:25 -0400

    adding caching via dalli

--- blarghhhh.rb
+++ blarghhhh.rb
@@ -8,6 +8,7 @@ require "sinatra"
 require "erb"
 require "rdiscount"
 require "builder"
+require "dalli"
 
 base_uri = 'http://github.com/api/v2/json'
 userid = 'zacharyscott'
@@ -15,21 +16,27 @@ repoid = 'my_blarghhhh'
 
 
 get '/' do
-  @hist_uri = base_uri + "/commits/list/" + userid + "/" + repoid + "/master"
-  @info = HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}")
-  @collaborators = HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}/collaborators")
-  @blobs = HTTParty.get("#{base_uri}/blob/all/#{userid}/#{repoid}/master")
-  erb :index
+	@dc = Dalli::Client.new('localhost:11211')
+	@dc.set('info', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}"))
+	@info = @dc.get('info')	
+	@dc.set('collaborators', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}/collaborators"))
+	@collaborators = @dc.get('collaborators')	
+  @dc.set('blobs', HTTParty.get("#{base_uri}/blob/all/#{userid}/#{repoid}/master"))
+	@blobs = @dc.get('blobs')	
+	erb :index
 end
 
 get '/show/:post/:sha' do
-  @info = HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}")
-  @collaborators = HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}/collaborators")
-  doc = HTTParty.get("#{base_uri}/blob/show/#{userid}/#{repoid}/#{params[:sha]}").to_s
-  @post = RDiscount.new(doc).to_html
-  
-  @history = HTTParty.get("#{base_uri}/commits/list/#{userid}/#{repoid}/master/#{params[:post]}").to_hash
-  erb :show
+	@dc = Dalli::Client.new('localhost:11211')
+	@dc.set('info', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}"))
+	@info = @dc.get('info')	
+	@dc.set('collaborators', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}/collaborators"))
+	@collaborators = @dc.get('collaborators')	
+	@dc.set(params[:sha], HTTParty.get("#{base_uri}/blob/show/#{userid}/#{repoid}/#{params[:sha]}").to_s)
+  @post = RDiscount.new(@dc.get(params[:sha])).to_html
+  @dc.set('history', HTTParty.get("#{base_uri}/commits/list/#{userid}/#{repoid}/master/#{params[:post]}").to_hash)
+ 	@history = @dc.get('history') 
+	erb :show
 end
 
 get '/stylesheet.css' do
@@ -38,9 +45,11 @@ get '/stylesheet.css' do
 end
 
 get '/rss.xml' do
-  @info = HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}")
-  @blobs = HTTParty.get("#{base_uri}/blob/all/#{userid}/#{repoid}/master")
-  
+	@dc = Dalli::Client.new('localhost:11211')
+	@dc.set('info', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}"))
+  @info = @dc.get('info')
+  @dc.set('blobs', HTTParty.get("#{base_uri}/blob/all/#{userid}/#{repoid}/master"))
+  @blobs = @dc.get('blobs')
   builder do |xml|
     xml.instruct! :xml, :version => '1.0'
     xml.rss :version => "2.0" do
@@ -50,8 +59,8 @@ get '/rss.xml' do
         xml.link @info["repository"]["homepage"]
         
         @blobs["blobs"].each_pair do |key, value|
-					hist = HTTParty.get("#{base_uri}/commits/list/#{userid}/#{repoid}/master/#{key}").to_hash
-          
+					@dc.set("hist-#{value}", HTTParty.get("#{base_uri}/commits/list/#{userid}/#{repoid}/master/#{key}").to_hash)
+         	hist = @dc.get("hist-#{value}") 
           xml.item do
             xml.title key
             xml.link "#{@info["repository"]["homepage"]}/show/#{key}/#{value}"            
