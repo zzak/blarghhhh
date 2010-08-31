c5a8a4ad7eafe95d0fb224f8052e1ddbea83ed43
Zachary Scott <zachary.s.scott@gmail.com>
2010-08-30 21:01:19 -0400

    switching rack dependency source from git to rubygems and modularizing the app

--- blarghhhh.rb
+++ blarghhhh.rb
@@ -4,73 +4,86 @@ Bundler.setup
 
 require "haml"
 require "httparty"
-require "sinatra"
+require "sinatra/base"
 require "erb"
 require "rdiscount"
 require "builder"
 require "dalli"
 
-base_uri = 'http://github.com/api/v2/json'
-userid = 'zacharyscott'
-repoid = 'my_blarghhhh'
+class Blarghhhh < Sinatra::Base
+
+  set :base_uri, 'http://github.com/api/v2/json'
+  set :userid, 'zacharyscott'
+  set :repoid, 'my_blarghhhh'
 
 
-get '/' do
-  @dc = Dalli::Client.new('localhost:11211')
-  @dc.set('info', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}"))
-  @info = @dc.get('info')	
-  @dc.set('collaborators', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}/collaborators"))
-  @collaborators = @dc.get('collaborators')	
-  @dc.set('blobs', HTTParty.get("#{base_uri}/blob/all/#{userid}/#{repoid}/master"))
-  @blobs = @dc.get('blobs')	
-  erb :index
-end
+  get '/' do
+    @dc = Dalli::Client.new('localhost:11211')
+    @dc.set('info', HTTParty.get(
+      "#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}"))
+    @info = @dc.get('info')	
+    @dc.set('collaborators', HTTParty.get(
+      "#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}/collaborators"))
+    @collaborators = @dc.get('collaborators')	
+    @dc.set('blobs', HTTParty.get(
+      "#{settings.base_uri}/blob/all/#{settings.userid}/#{settings.repoid}/master"))
+    @blobs = @dc.get('blobs')	
+    erb :index
+  end
 
-get '/show/:post/:sha' do
-  @dc = Dalli::Client.new('localhost:11211')
-  @dc.set('info', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}"))
-  @info = @dc.get('info')	
-  @dc.set('collaborators', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}/collaborators"))
-  @collaborators = @dc.get('collaborators')	
-  @dc.set(params[:sha], HTTParty.get("#{base_uri}/blob/show/#{userid}/#{repoid}/#{params[:sha]}").to_s)
-  @post = RDiscount.new(@dc.get(params[:sha])).to_html
-  @dc.set('history', HTTParty.get("#{base_uri}/commits/list/#{userid}/#{repoid}/master/#{params[:post]}").to_hash)
-  @history = @dc.get('history') 
-  erb :show
-end
+  get '/show/:post/:sha' do
+    @dc = Dalli::Client.new('localhost:11211')
+    @dc.set('info', HTTParty.get(
+      "#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}"))
+    @info = @dc.get('info')	
+    @dc.set('collaborators', HTTParty.get(
+      "#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}/collaborators"))
+    @collaborators = @dc.get('collaborators')	
+    @dc.set(params[:sha], HTTParty.get(
+      "#{settings.base_uri}/blob/show/#{settings.userid}/#{settings.repoid}/#{params[:sha]}").to_s)
+    @post = RDiscount.new(@dc.get(params[:sha])).to_html
+    @dc.set('history', HTTParty.get(
+      "#{settings.base_uri}/commits/list/#{settings.userid}/#{settings.repoid}/master/#{params[:post]}").to_hash)
+    @history = @dc.get('history') 
+    erb :show
+  end
 
-get '/stylesheet.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  sass :stylesheet
-end
+  get '/stylesheet.css' do
+    content_type 'text/css', :charset => 'utf-8'
+    sass :stylesheet
+  end
 
-get '/rss.xml' do
-  @dc = Dalli::Client.new('localhost:11211')
-  @dc.set('info', HTTParty.get("#{base_uri}/repos/show/#{userid}/#{repoid}"))
-  @info = @dc.get('info')
-  @dc.set('blobs', HTTParty.get("#{base_uri}/blob/all/#{userid}/#{repoid}/master"))
-  @blobs = @dc.get('blobs')
-  builder do |xml|
-    xml.instruct! :xml, :version => '1.0'
-    xml.rss :version => "2.0" do
-      xml.channel do
-        xml.title @info["repository"]["name"]
-        xml.description @info["repository"]["description"]
-        xml.link @info["repository"]["homepage"]
-        
-        @blobs["blobs"].each_pair do |key, value|
-          @dc.set("hist-#{value}", HTTParty.get("#{base_uri}/commits/list/#{userid}/#{repoid}/master/#{key}").to_hash)
-          hist = @dc.get("hist-#{value}") 
-          xml.item do
-            xml.title key
-            xml.link "#{@info["repository"]["homepage"]}/show/#{key}/#{value}"            
-            xml.guid "#{@info["repository"]["homepage"]}/show/#{key}/#{value}"
-            xml.pubDate Time.parse("#{hist["commits"][0]["authored_date"]}".to_s).rfc822()	
+  get '/rss' do
+    @dc = Dalli::Client.new('localhost:11211')
+    @dc.set('info', HTTParty.get(
+      "#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}"))
+    @info = @dc.get('info')
+    @dc.set('blobs', HTTParty.get(
+      "#{settings.base_uri}/blob/all/#{settings.userid}/#{settings.repoid}/master"))
+    @blobs = @dc.get('blobs')
+    builder do |xml|
+      xml.instruct! :xml, :version => '1.0'
+      xml.rss :version => "2.0" do
+        xml.channel do
+          xml.title @info["repository"]["name"]
+          xml.description @info["repository"]["description"]
+          xml.link @info["repository"]["homepage"]
+          
+          @blobs["blobs"].each_pair do |key, value|
+            @dc.set("hist-#{value}", HTTParty.get(
+              "#{settings.base_uri}/commits/list/#{settings.userid}/#{settings.repoid}/master/#{key}").to_hash)
+            hist = @dc.get("hist-#{value}") 
+            xml.item do
+              xml.title key
+              xml.link "#{@info["repository"]["homepage"]}/show/#{key}/#{value}"            
+              xml.guid "#{@info["repository"]["homepage"]}/show/#{key}/#{value}"
+              xml.pubDate Time.parse("#{hist["commits"][0]["authored_date"]}".to_s).rfc822()	
+            end
           end
         end
       end
     end
   end
-end
 
+end
 
