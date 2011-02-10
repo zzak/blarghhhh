edcd894fe2e2483d7d68842ee4bc681e4d056c21
Zachary Scott <zachary.s.scott@gmail.com>
2011-02-10 01:20:46 -0500

    haml layout

--- blarghhhh.rb
+++ blarghhhh.rb
@@ -2,10 +2,10 @@ require "rubygems"
 require "bundler"
 Bundler.setup
 
+require "haml"
 require "sass"
 require "httparty"
 require "sinatra/base"
-require "erb"
 require "rdiscount"
 require "dalli"
 require "yaml"
@@ -15,67 +15,54 @@ class Blarghhhh < Sinatra::Base
   set :base_uri, 'http://github.com/api/v2/json'
   set :userid, ENV['GITHUB_USER']
   set :repoid, ENV['GITHUB_REPO']
-  
   set :public, File.dirname(__FILE__) + '/public'
-
   set :cache, Dalli::Client.new(ENV['MEMCACHE_SERVERS'], :username => ENV['MEMCACHE_USERNAME'], :password => ENV['MEMCACHE_PASSWORD'], :expires_in => 300)
 
   get '/' do
     @info = settings.cache.fetch("info") do
       HTTParty.get("#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}")
     end
-    
     @collaborators = settings.cache.fetch("collaborators") do
       HTTParty.get("#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}/collaborators")
     end
-
     @branches = settings.cache.fetch("branches") do
       HTTParty.get("#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}/branches")
     end
-    
-    erb :index
+    haml :index
   end
 
   get '/b/:branch' do
     @info = settings.cache.fetch("info") do
       HTTParty.get("#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}")
     end
-     
     @collaborators = settings.cache.fetch("collaborators") do
       HTTParty.get("#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}/collaborators")
     end
-     
     @blobs = settings.cache.fetch("#{params[:branch]}-blobs") do
       HTTParty.get("#{settings.base_uri}/blob/all/#{settings.userid}/#{settings.repoid}/#{params[:branch]}")
     end	
-
-    erb :branch
+    haml :branch
   end
 
   get '/show/:branch/:post/:sha' do
     @info = settings.cache.fetch("info") do
       HTTParty.get("#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}")
     end
-    
     @collaborators = settings.cache.fetch("collaborators") do
       HTTParty.get("#{settings.base_uri}/repos/show/#{settings.userid}/#{settings.repoid}/collaborators")
     end
-    
     markdown = settings.cache.fetch("#{params[:sha]}") do
       HTTParty.get("#{settings.base_uri}/blob/show/#{settings.userid}/#{settings.repoid}/#{params[:sha]}").to_s
     end
     @post = RDiscount.new(markdown).to_html
-    
     @history = settings.cache.fetch("#{params[:sha]}-history") do
       HTTParty.get("#{settings.base_uri}/commits/list/#{settings.userid}/#{settings.repoid}/#{params[:branch]}/#{params[:post]}").to_hash
     end
-    
-    erb :show
+    haml :show
   end
 
   get '/stylesheet.css' do
     content_type 'text/css', :charset => 'utf-8'
     sass :stylesheet
   end
-
 end
